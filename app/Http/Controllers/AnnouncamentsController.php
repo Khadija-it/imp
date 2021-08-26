70533f36329515d4b2a32731d57badeabb1b5490
Khadija-it <57592538+Khadija-it@users.noreply.github.com>
2021-08-26 18:00:39 +0300

    Update AnnouncamentsController.php

--- app/Http/Controllers/AnnouncamentsController.php
+++ app/Http/Controllers/AnnouncamentsController.php
@@ -15,52 +15,26 @@ class AnnouncamentsController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('Announcaments.index')->with('announcaments',Announcament::all());
-
-        
-       
+        return view('Announcaments.index')->with('announcaments',Announcament::all());   
     }
-
     /**
      * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
         return view('Announcaments.create');
     }
-
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
     public function store(announcamentsRequest $request)
     {
-    
-
-
-        Announcament::create($request->all());
-       
-
-        return redirect( route('home'))->with('success', 'Post created Successfully!');
-
-        
+            Announcament::create($request->all());
+               return redirect( route('home'))->with('success', 'Post created Successfully!');    
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         return view('request')->with('announcaments',Announcament::all());
@@ -69,9 +43,6 @@ class AnnouncamentsController extends Controller
    
     /**
      * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
